afb5bda0376eaa3e7229a56b38a5cefa520ac542
Steven <stevenlu1997@yahoo.com>
2020-04-17 15:29:47 -0700

    Initial Commit

---
+++ JCHs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JCHs
+//
+//  Created by Steven Lu on 4/17/20.
+//  Copyright © 2020 Steven Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
