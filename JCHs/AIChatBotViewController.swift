12686171920be55954c7eb6f14283f732996e7cf
Steven <stevenlu1997@yahoo.com>
2020-04-21 16:03:23 -0700

    Working 2nd UI

--- JCHs/AIChatBotViewController.swift
+++ JCHs/AIChatBotViewController.swift
@@ -1,11 +1,16 @@
-
+//
+//  AIChatBotViewController.swift
+//  JCHs
+//
+//  Created by Steven Lu on 4/21/20.
+//  Copyright © 2020 Steven Lu. All rights reserved.
+//
 
 import UIKit
 
-class AIChatBotViewController: UITableViewController {
+class AIChatBotViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 }
