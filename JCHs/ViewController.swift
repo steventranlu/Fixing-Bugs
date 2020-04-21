2382318107ff69631ee1959aae7057e57d5ace09
Steven <stevenlu1997@yahoo.com>
2020-04-21 13:18:05 -0700

    Sound Effects. Completed.

--- JCHs/ViewController.swift
+++ JCHs/ViewController.swift
@@ -9,12 +9,30 @@ class ViewController: UIViewController {
     var audioPlayer: AVAudioPlayer!
     
     
+    @IBAction func switchPlaySoundAction(_ sender: UISwitch) {
+        if let playAudioPlayer = audioPlayer {
+            if !switchPlaySound.isOn {
+                playAudioPlayer.stop()
+            }
+        }
+    }
+    
+    let arraySounds = ["Sound0"]
     
     
-    
-    
-    
-    
+    func playSound(soundName:String){
+        if let sound = NSDataAsset(name: soundName){
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            }catch {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            }else {
+                print("Error: Could not read data from song files")
+            }
+        }
+
     
     
     
@@ -25,6 +43,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        playSound(soundName: arraySounds[0])
 
     }
 
