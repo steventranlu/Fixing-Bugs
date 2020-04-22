a05c96b768e214ab9c5af9a8ff72c170a2c1436f
Steven <stevenlu1997@yahoo.com>
2020-04-21 17:16:55 -0700

    10% Done with Customer Service UI.

---
+++ JCHs/CustomerServiceViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CustomerServiceViewController.swift
+//  JCHs
+//
+//  Created by Steven Lu on 4/21/20.
+//  Copyright © 2020 Steven Lu. All rights reserved.
+//
+
+import UIKit
+
+class CustomerServiceViewController: UIViewController {
+
+
+    @IBOutlet weak var buttonCallUs: UIButton!
+    @IBOutlet weak var buttonMessageUs: UIButton!
+    @IBOutlet weak var buttonSendUsPhotos: UIButton!
+    
+    @IBOutlet weak var imageUnavailable: UIImageView!
+    @IBOutlet weak var imageTop: UIImageView!
+    @IBOutlet weak var imageMiddle: UIImageView!
+    @IBOutlet weak var imageBottom: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageUnavailable.image = UIImage(named: "")
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
