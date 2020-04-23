11cf447f278b39a0a7142338bbe448a99c32bb25
Steven <stevenlu1997@yahoo.com>
2020-04-23 16:14:06 -0700

    Take Notes VC.

---
+++ JCHs/TakeNotesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TakeNotesViewController.swift
+//  JCHs
+//
+//  Created by Steven Lu on 4/23/20.
+//  Copyright © 2020 Steven Lu. All rights reserved.
+//
+
+import UIKit
+
+class TakeNotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
