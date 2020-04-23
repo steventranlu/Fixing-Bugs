e6dab543788eaee96ba9c812d67172c808f00d49
Steven <stevenlu1997@yahoo.com>
2020-04-23 16:41:17 -0700

    Takes Notes II Complete.

--- JCHs/TakeNotesViewController.swift
+++ JCHs/TakeNotesViewController.swift
@@ -1,11 +1,3 @@
-//
-//  TakeNotesViewController.swift
-//  JCHs
-//
-//  Created by Steven Lu on 4/23/20.
-//  Copyright © 2020 Steven Lu. All rights reserved.
-//
-
 import UIKit
 
 class TakeNotesViewController: UIViewController {
