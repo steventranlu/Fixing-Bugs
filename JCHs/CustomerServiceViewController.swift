05f2360d1e24a3797987b0929575429acad12a30
Steven <stevenlu1997@yahoo.com>
2020-04-23 19:32:02 -0700

    1-2 More UI to go.

--- JCHs/CustomerServiceViewController.swift
+++ JCHs/CustomerServiceViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class CustomerServiceViewController: UIViewController {
 
 
+    @IBOutlet weak var textField: UITextView!
     @IBOutlet weak var buttonCallUs: UIButton!
     @IBOutlet weak var buttonMessageUs: UIButton!
     @IBOutlet weak var buttonSendUsPhotos: UIButton!
@@ -19,6 +20,7 @@ class CustomerServiceViewController: UIViewController {
     @IBOutlet weak var imageTop: UIImageView!
     @IBOutlet weak var imageMiddle: UIImageView!
     @IBOutlet weak var imageBottom: UIImageView!
+    @IBOutlet weak var imageFour: UIImageView!
     
     @IBOutlet weak var labelUnavailable: UILabel!
     
@@ -35,6 +37,9 @@ class CustomerServiceViewController: UIViewController {
         imageTop.image = UIImage(named: "")
         imageMiddle.image = UIImage(named: "")
         imageBottom.image = UIImage(named: "")
+        imageFour.image = UIImage(named: "")
+        textField.text = ""
+
     }
     
     @IBAction func buttonMessageUsNull(_ sender: UIButton) {
@@ -43,6 +48,8 @@ class CustomerServiceViewController: UIViewController {
         imageTop.image = UIImage(named: "")
         imageMiddle.image = UIImage(named: "")
         imageBottom.image = UIImage(named: "")
+        imageFour.image = UIImage(named: "")
+        textField.text = ""
     }
     
     @IBAction func buttonSendUsPhotos(_ sender: UIButton) {
@@ -51,6 +58,8 @@ class CustomerServiceViewController: UIViewController {
         imageTop.image = UIImage(named: "")
         imageMiddle.image = UIImage(named: "")
         imageBottom.image = UIImage(named: "")
+        imageFour.image = UIImage(named: "")
+        textField.text = ""
     }
     
     
@@ -60,6 +69,8 @@ class CustomerServiceViewController: UIViewController {
         imageTop.image = UIImage(named: "2")
         imageMiddle.image = UIImage(named: "3")
         imageBottom.image = UIImage(named: "4")
+        imageFour.image = UIImage(named: "22")
+        textField.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
         
     }
     
