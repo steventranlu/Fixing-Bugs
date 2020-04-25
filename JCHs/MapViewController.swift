00afaf3d93cdfc5f12c0a1ac7e6d7b4ac360e86d
Steven <stevenlu1997@yahoo.com>
2020-04-25 14:31:33 -0700

    MAP UI working!!

---
+++ JCHs/MapViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MapViewController.swift
+//  JCHs
+//
+//  Created by Steven Lu on 4/25/20.
+//  Copyright © 2020 Steven Lu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        let userLocation = locations.last
+        let viewRegion = MKCoordinateRegion(center: (userLocation?.coordinate)!, latitudinalMeters: 600, longitudinalMeters: 600)
+        self.map.setRegion(viewRegion, animated: true)
+    }
+
+
+}
