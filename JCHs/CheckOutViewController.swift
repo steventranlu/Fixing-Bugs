eb1254d022b36b921104ab3ee8b02515dfd2ee97
Steven <stevenlu1997@yahoo.com>
2020-05-08 16:49:18 -0700

    Close Segue works!

---
+++ JCHs/CheckOutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CheckOutViewController.swift
+//  JCHs
+//
+//  Created by Steven Lu on 5/8/20.
+//  Copyright © 2020 Steven Lu. All rights reserved.
+//
+
+import UIKit
+
+class CheckOutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func myUnwindFunction(unwindSegue: UIStoryboardSegue){
+        
+    }
+    @IBAction func closeClicked(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
